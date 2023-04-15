17b3cdbded089c48467099cdce91ce6541659073
siddubasha <86608891+siddubasha@users.noreply.github.com>
2023-04-15 09:19:56 +0530

    springboot-rest-api-with-modelmapper-library

--- springboot-restful-webservices/src/main/java/com/siddu/webapp/service/impl/UserServiceImpl.java
+++ springboot-restful-webservices/src/main/java/com/siddu/webapp/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.siddu.webapp.dto.UserDto;
@@ -19,13 +20,16 @@ import lombok.AllArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
+	private ModelMapper modelMapper;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
         
-		User user=UserMapper.mapToUser(userDto);
+	//	User user=UserMapper.mapToUser(userDto);
+		User user=modelMapper.map(userDto, User.class);
 		User savedUser= userRepository.save(user);
-		UserDto savedUserdto=UserMapper.mapToUserDto(savedUser);
+	//	UserDto savedUserdto=UserMapper.mapToUserDto(savedUser);
+		UserDto savedUserdto=modelMapper.map(savedUser, UserDto.class);
 		return savedUserdto;
 	}
 
@@ -33,7 +37,8 @@ public class UserServiceImpl implements UserService {
 	public UserDto getUserById(Long userId) {
 		Optional<User> optionalUser = userRepository.findById(userId);
 		 User user=optionalUser.get();
-		return UserMapper.mapToUserDto(user);
+		//return UserMapper.mapToUserDto(user);
+		 return modelMapper.map(user, UserDto.class);
 	}
 
 	@Override
@@ -41,7 +46,8 @@ public class UserServiceImpl implements UserService {
 
 		List<User> users= userRepository.findAll();
 		
-		return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+	//	return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+		return users.stream().map((user)-> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
 	}
 
 	@Override
@@ -51,7 +57,8 @@ public class UserServiceImpl implements UserService {
 		existingUser.setLastName(user.getLastName());
 		existingUser.setEmail(user.getEmail());
 		User updatedUser = userRepository.save(existingUser);
-		return UserMapper.mapToUserDto(updatedUser);
+	//	return UserMapper.mapToUserDto(updatedUser);
+		return modelMapper.map(updatedUser, UserDto.class);
 	}
 
 	@Override
